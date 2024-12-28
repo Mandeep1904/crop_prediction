212806fd7e7be9d45f6d90b54fbed6669fd1c21e
Mandeep1904 <mandeepgohil19@gmail.com>
2024-12-28 22:58:50 +0530

    fix: correct prediction output format in render_template

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def predict():
     float_features = [float(x) for x in request.form.values()]
     features = [np.array(float_features)]
     prediction = model.predict(features)
-    return render_template("index.ejs", prediction_text = "The Predicted Crop is {}".format(prediction))
+    return render_template("index.ejs", prediction_text = "The Predicted Crop is {}".format(prediction[0]))
+
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
