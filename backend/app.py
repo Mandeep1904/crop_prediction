e401d4ecf11042d96f1eee617548fabec16b1559
Mandeep1904 <mandeepgohil19@gmail.com>
2025-01-17 19:19:49 +0530

    fix: update CORS configuration in app.py to use only FRONTEND_URL

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,8 @@ app = Flask(__name__)
 from flask_cors import CORS
 
 # CORS(app)
-CORS(app, origins=['http://localhost:5173', FRONTEND_URL])
+# CORS(app, origins=['http://localhost:5173', FRONTEND_URL])
+CORS(app, origins=[FRONTEND_URL])
 
 model = pickle.load(open("model.pkl", "rb"))
 
